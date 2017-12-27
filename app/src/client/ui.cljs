8c5d38ad100bf5e0a12b40a951bd243cee4f4f31
Alexis Vincent <mail@alexisvincent.io>
2017-12-27 17:54:37 +0200

    Added wealth tracker

--- app/src/client/ui.cljs
+++ app/src/client/ui.cljs
@@ -4,6 +4,8 @@
    [rum.core :as rum]
    [ajax.core :refer [GET]]
    [cljs.spec.alpha :as s]
+	 [goog.string :as gstring]
+	 [goog.string.format]
    ;; [cljss.core :as cljss :refer-macros [defstyles]]
 	 ))
 
@@ -21,6 +23,9 @@
 (def zar-starting (atom 100000))
 (def zar-usd (atom 1))
 (def zar-eur (atom 1))
+
+(defn format [x]
+	(gstring/format "%.2f" x))
 
 (defn update-luno-btc-zar []
   (GET (bypass-cors "https://api.mybitx.com/api/1/ticker?pair=XBTZAR")
@@ -40,7 +45,6 @@
 (defn update-coinbase-btc-eur []
   (GET "https://api.coinbase.com/v2/exchange-rates?currency=BTC"
 			 :handler (fn [{{{rate "EUR"} "rates"} "data"}]
-									(js/console.log rate)
 									(reset! coinbase-btc-eur (js/parseInt rate)))))
 
 (defn update-zar-eur []
@@ -72,6 +76,38 @@
 
 (refresh-all)
 
+
+(rum/defc investment []
+	(let [dylan-investment 22700
+				alexis-investment 103500
+				sharon-investment 600
+				balance-1 (+ alexis-investment dylan-investment sharon-investment)
+				dylan-percentage (/ dylan-investment balance-1)
+				alexis-percentage (/ alexis-investment balance-1)
+				sharon-percentage (/ sharon-investment balance-1)
+				balance-last balance-1
+				profit (- balance-last balance-1)
+				growth (format (* 100 (- (/ balance-last balance-1) 1)))
+				]
+		[:div {:style {:border-style "solid"}}
+		 [:h2 "R" balance-last " ( R" profit " profit @" growth "% )"]
+
+		 [:div {:style {:border-style "dotted"}}
+			[:h3 "Alexis Vincent"]
+			[:h4 "Totol Assets R" (format  (* alexis-percentage balance-last))]
+			[:h4 "Total Profits R" (format (* alexis-percentage profit))]]
+
+		 [:div {:style {:border-style "dotted"}}
+			[:h3 "Sharon Vincent"]
+			[:h4 "Total Assets R" (format (* sharon-percentage balance-last))]
+			[:h4 "Total Profits R" (format (* sharon-percentage profit))]]
+
+		 [:div {:style {:border-style "dotted"}}
+			[:h3 "Dylan Vorster"]
+			[:h4 "Total Assets R" (format (* dylan-percentage balance-last))]
+			[:h4 "Total Profits R" (format (* dylan-percentage profit))]]
+		 ]))
+
 (rum/defc root < rum/reactive []
 
   (let [get-arbitrage-potential
@@ -84,22 +120,30 @@
 
     [:div
 
-     (connected-input "ZAR starting" zar-starting #())
-     (connected-input "Luno - BTC/ZAR" luno-btc-zar update-luno-btc-zar)
-     (connected-input "Bitstamp - BTC/USD" bitstamp-btc-usd update-bitstamp-btc-usd)
-     (connected-input "Coinbase - BTC/EUR" coinbase-btc-eur update-coinbase-btc-eur)
-     (connected-input "Quoinex - BTC/USD" quoinex-btc-usd update-quoinex-btc-usd)
-     (connected-input "ZAR/USD" zar-usd update-zar-usd)
-     (connected-input "ZAR/EUR" zar-eur update-zar-eur)
+		 [:div {:style {:display "flex" :flex-wrap "wrap"}}
+			[:div
+			 (connected-input "ZAR starting" zar-starting #())
+			 (connected-input "Luno - BTC/ZAR" luno-btc-zar update-luno-btc-zar)
+			 (connected-input "Bitstamp - BTC/USD" bitstamp-btc-usd update-bitstamp-btc-usd)
+			 (connected-input "Coinbase - BTC/EUR" coinbase-btc-eur update-coinbase-btc-eur)
+			 (connected-input "Quoinex - BTC/USD" quoinex-btc-usd update-quoinex-btc-usd)
+			 (connected-input "ZAR/USD" zar-usd update-zar-usd)
+			 (connected-input "ZAR/EUR" zar-eur update-zar-eur)
+			 [:button {:on-click refresh-all} "refresh all"]
+			 ]
 
-		 (let [[percentage profit] (get-arbitrage-potential (rum/react zar-usd) (rum/react bitstamp-btc-usd))]
-		 	 [:h1 "USD - Bitstamp " percentage "%"])
+			[:div
+			 (let [[percentage profit] (get-arbitrage-potential (rum/react zar-usd) (rum/react bitstamp-btc-usd))]
+				 [:h1 "USD - Bitstamp " (format percentage) "%"])
 
-		 (let [[percentage profit] (get-arbitrage-potential (rum/react zar-usd) (rum/react quoinex-btc-usd))]
-		 	 [:h1 "USD - Quoinex " percentage "%"])
+			 (let [[percentage profit] (get-arbitrage-potential (rum/react zar-usd) (rum/react quoinex-btc-usd))]
+				 [:h1 "USD - Quoinex " (format percentage) "%"])
 
-		 (let [[percentage profit] (get-arbitrage-potential (rum/react zar-eur) (rum/react coinbase-btc-eur))]
-		 	 [:h1 "EUR - Coinbase " percentage "%"])
+			 (let [[percentage profit] (get-arbitrage-potential (rum/react zar-eur) (rum/react coinbase-btc-eur))]
+				 [:h1 "EUR - Coinbase " (format percentage) "%"])
+			 ]
 
+			]
 
-     [:button {:on-click refresh-all} "refresh all"]]))
+		 (investment)
+		 ]))
